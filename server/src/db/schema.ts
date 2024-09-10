e8e7447bb5d5e575500909358e33d54e1c35d53b
danielstos <daniel.jose.stos@gmail.com>
2024-09-10 07:58:16 -0300

    feat: create goalCompletions table and add cuid2

--- server/src/db/schema.ts
+++ server/src/db/schema.ts
@@ -1,9 +1,24 @@
 import { pgTable, text, integer, timestamp } from "drizzle-orm/pg-core";
+import { createId } from "@paralleldrive/cuid2";
 
 export const goals = pgTable("goals", {
-  id: text("id").primaryKey(),
+  id: text("id")
+    .primaryKey()
+    .$defaultFn(() => createId()),
   title: text("title").notNull(),
   desiredWeeklyFrequency: integer("desired_weekly_frequency").notNull(),
+  createdAt: timestamp("created_at", { withTimezone: true })
+    .defaultNow()
+    .notNull(),
+});
+
+export const goalCompletions = pgTable("goal_completions", {
+  id: text("id")
+    .primaryKey()
+    .$defaultFn(() => createId()),
+  goalId: text("goal_id")
+    .references(() => goals.id)
+    .notNull(),
   createdAt: timestamp("created_at", { withTimezone: true })
     .defaultNow()
     .notNull(),
