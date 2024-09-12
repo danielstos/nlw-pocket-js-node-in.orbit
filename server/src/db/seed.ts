d81e65029810f06d7a054a3c8b6c772736031cbe
danielstos <daniel.jose.stos@gmail.com>
2024-09-12 06:39:05 -0300

    feat: Creating CreateGoal for goals route
    improvement with Fastify Type Provider Zod in server.ts

--- server/src/db/seed.ts
+++ server/src/db/seed.ts
@@ -1,14 +1,27 @@
-import { db } from ".";
+import { client, db } from ".";
 import { goalCompletions, goals } from "./schema";
+import dayjs from "dayjs";
 
 async function seed() {
   await db.delete(goalCompletions);
   await db.delete(goals);
 
-  await db.insert(goals).values([
-    { title: "Acordar cedo", desiredWeeklyFrequency: 5 },
-    { title: "Me exercitar", desiredWeeklyFrequency: 3 },
-    { title: "Meditar", desiredWeeklyFrequency: 1 },
+  const result = await db
+    .insert(goals)
+    .values([
+      { title: "Acordar cedo", desiredWeeklyFrequency: 5 },
+      { title: "Me exercitar", desiredWeeklyFrequency: 3 },
+      { title: "Meditar", desiredWeeklyFrequency: 1 },
+    ])
+    .returning();
+
+  const starOfWeek = dayjs().startOf("week");
+
+  await db.insert(goalCompletions).values([
+    { goalId: result[0].id, createdAt: starOfWeek.toDate() },
+    { goalId: result[1].id, createdAt: starOfWeek.add(1, "day").toDate() },
   ]);
 }
-seed();
+seed().finally(() => {
+  client.end();
+});
--- server/src/http/server.ts
+++ server/src/http/server.ts
@@ -1,6 +1,36 @@
 import fastify from "fastify";
+import {
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from "fastify-type-provider-zod";
+import { createGoal } from "../functions/create-goal";
+import z from "zod";
 
-const app = fastify();
+const app = fastify().withTypeProvider<ZodTypeProvider>();
+// Add schema validator and serializer
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+app.post(
+  "/goals",
+  {
+    schema: {
+      body: z.object({
+        title: z.string(),
+        desiredWeeklyFrequency: z.number().int().min(1).max(7),
+      }),
+    },
+  },
+  async (request) => {
+    const { title, desiredWeeklyFrequency } = request.body;
+    await createGoal({
+      title,
+      desiredWeeklyFrequency,
+    });
+  }
+);
+
 app
   .listen({
     port: 3333,
